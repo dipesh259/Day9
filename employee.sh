8cc888b0f1a663f050de105abec2de14a7e6ee46
sick3356 <myselfdipu259@gmail.com>
2021-08-23 09:39:01 +0530

    UC3

--- employee.sh
+++ employee.sh
@@ -10,15 +10,15 @@ randomNumber=$((RANDOM%2))
 if [ $randomNumber -eq 	$isPresent ]
 then
 	echo "Employee Present"
+	
+#TO FUNCTION PART TIME EMPLOYEE AND  WAGE
+	function partTimeEmployeeAndWage(){
+	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+	read -p "To part time hour is :" isFullDayHour
+	calculatePartTimeEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage "rupees"
+}
+partTimeEmployeeAndWage
 else
 	echo "Employee Absent"
 fi
-
-#TO FUNCTION CALCULATE EMPLOYEE WAGE
-function calculateDailyEmployeeWage(){
-	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
-	read -p "To full day hours is :" isFullDayHour
-	calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
-	echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
-}
-calculateDailyEmployeeWage
